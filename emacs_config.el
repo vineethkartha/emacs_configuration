59729bd01e26a703009d9d23dec72f905cae5096
vineethkartha <vineeth.kartha@gmail.com>
2019-12-15 16:28:25 +0530

    updated the configuration

--- emacs_config.el
+++ emacs_config.el
@@ -1,92 +1,103 @@
-(custom-set-variables
-'(inhibit-startup-screen t))
-
+(require 'package)
+(package-initialize)
 (add-to-list 'package-archives
 	     '("melpa" . "https://stable.melpa.org/packages/"))
 (add-to-list 'package-archives
 	     '("org" . "http://orgmode.org/elpa/"))
 
-(unless package-archive-contents
+(package-initialize)
+(when (not package-archive-contents)  
   (package-refresh-contents))
 
-(package-initialize)
+(defvar my-packages
+  '(
+    use-package
+    elpy
+    flycheck
+    ace-window
+    org-bullets
+    powerline
+    )
+  )
 
-;; if not yet installed, install package use-package
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
+(dolist (pkg my-packages)
+  (unless (package-installed-p pkg)
+    (package-install pkg)))
 
-(setq pfl-packages
-        '(
-            company
-            company-c-headers
-            company-irony
-            company-quickhelp
-	    company-rtags
-	    flycheck
-	    flycheck-rtags
-	    powerline
-	    org-bullets
-	    rtags
-	    srefactor
-	    origami
-	    ace-window
-            ))
+(defun enable-flycheck()
+  (flycheck-mode 1)
+  )
 
-	    (dolist (pkg pfl-packages)
-	    (when (and (not (package-installed-p pkg)) (assoc pkg package-archive-contents))
-	    (package-install pkg)))
+(defun set-local-key-for-hs-mode()
+  (hs-minor-mode 1)
+  (local-set-key (kbd "M-;") 'hs-toggle-hiding)
+  )
+
+(defun copy-line-above ()
+  (interactive)
+  (previous-line)
+  (setq line (buffer-substring-no-properties
+	      (line-beginning-position)
+	      (line-end-position))
+	)
+  (next-line)
+  (insert line)
+  )
 
 (global-linum-mode 1)
-(global-flycheck-mode)
-(powerline-default-theme)
+(ido-mode 1)
+(electric-pair-mode 1)
+(use-package powerline
+  :ensure t
+  :config
+  (powerline-default-theme)
+  )
+
+(use-package company
+  :ensure t
+  :config
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 2)
+  (setq global-company-mode 1))
+
+(use-package company-irony
+  :ensure t
+  :config
+  (require 'company)
+  (add-to-list 'company-backends 'company-irony)
+  )
+
+;sudo apt-get install libclang-9-dev
+;M-x irony-install-server
+(use-package irony
+  :ensure t
+  :config
+  (add-hook 'c++-mode-hook 'irony-mode)
+  (add-hook 'c-mode-hook 'irony-mode)
+  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
+
+(with-eval-after-load 'company
+  (add-hook 'c++-mode-hook 'company-mode)
+  (add-hook 'c-mode-hook 'company-mode)
+  (add-hook 'emacs-lisp-mode-hook 'company-mode)
+  )
+  (add-hook 'c++-mode-hook (
+		   lambda()
+			 (enable-flycheck)
+			 (set-local-key-for-hs-mode)
+			 ))
+
+(elpy-enable)
+(setq elpy-rpc-virtualenv-path 'current)
+
+(use-package ace-window
+  :ensure t
+  :config
+  (global-set-key (kbd "M-o") 'ace-window)
+  (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+  )
 
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-(setq org-ellipsis " â¤µ")
+(setq org-ellipsis "->")
 (org-babel-do-load-languages
- 'org-babel-load-languages '((C . t) (shell . t)))
-
-(defun setup-flycheck-rtags ()
-(interactive)
-(flycheck-select-checker 'rtags)
-;; RTags creates more accurate overlays.
-(setq-local flycheck-highlighting-mode nil)
-(setq-local flycheck-check-syntax-automatically nil))
-
-;; only run this if rtags is installed
-(when (require 'rtags nil :noerror)
-;; make sure you have company-mode installed
-(require 'company)
-(define-key c-mode-base-map (kbd "M-.")
-(function rtags-find-symbol-at-point))
-(define-key c-mode-base-map (kbd "M-,")
-(function rtags-find-references-at-point))
-;; disable prelude's use of C-c r, as this is the rtags keyboard prefix
-;;(define-key prelude-mode-map (kbd "C-c r") nil)
-;; install standard rtags keybindings. Do M-. on the symbol below to
-;; jump to definition and see the keybindings.
-(rtags-enable-standard-keybindings)
-;; comment this out if you don't have or don't use helm
-;;(setq rtags-use-helm t)
-;; company completion setup
-(setq rtags-autostart-diagnostics t)
-(rtags-diagnostics)
-(setq rtags-completions-enabled t)
-(push 'company-rtags company-backends)
-(global-company-mode)
-(define-key c-mode-base-map (kbd "<C-tab>") (function company-complete))
-;; use rtags flycheck mode -- clang warnings shown inline
-(require 'flycheck-rtags)
-;; c-mode-common-hook is also called by c++-mode
-(add-hook 'c-mode-common-hook #'setup-flycheck-rtags))
-
-(semantic-mode 1) ;; -> this is optional for Lisp
-
-(define-key c-mode-map (kbd "M-RET") 'srefactor-refactor-at-point)
-(define-key c++-mode-map (kbd "M-RET") 'srefactor-refactor-at-point)
-(global-set-key (kbd "M-RET o") 'srefactor-lisp-one-line)
-(global-set-key (kbd "M-RET m") 'srefactor-lisp-format-sexp)
-(global-set-key (kbd "M-RET d") 'srefactor-lisp-format-defun)
-(global-set-key (kbd "M-RET b") 'srefactor-lisp-format-buffer)
-
-(global-set-key (kbd "M-o") 'ace-window)
-(setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+'org-babel-load-languages '((C . t) (shell . t)))
